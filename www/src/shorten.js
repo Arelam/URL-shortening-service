a39fb27c6b487d03f5539496f7f978ce40f22e25
Are Kristoffer Lamo <arelamo@studioare.net>
2020-09-09 22:03:00 +0200

    JS: Random string generation only AZ-az

--- www/src/shorten.js
+++ www/src/shorten.js
@@ -36,12 +36,14 @@ function formatEntry(full, short) {
 
 function generateShortURL(stringLength) {
 	let random = '';
-	for(let i=0;i<stringLength;i++) {
+	while(random.length<stringLength) {
 		// https://asecuritysite.com/coding/asc2
 		// 65(A)-122(z)=57 possible
 		let s = Math.floor(Math.random()*57);
-		random += String.fromCharCode((s+65));
-		console.log(s+65);
+		let sChar = s+65
+		if(sChar<91 || sChar>96) {
+			random += String.fromCharCode(sChar);
+		}
 	}
 	return random;
 }
