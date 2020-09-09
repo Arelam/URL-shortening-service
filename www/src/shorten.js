f8633bad649aedec3a6f1cce16542e5bcbcf632d
Are Kristoffer Lamo <arelamo@studioare.net>
2020-09-09 23:48:46 +0200

    JS: Creation time format finalized

--- www/src/shorten.js
+++ www/src/shorten.js
@@ -36,22 +36,32 @@ function addEntry(url) {
 	do {
 		shortURL = generateShortURL(4);
 	} while (entries.some(entries => entries.shortURL === shortURL));
-	
+
 	let format = formatEntry(url, shortURL);
 	document.getElementById('table-entries').appendChild(format);
 	entries.push({url, shortURL});
 }
 
 function formatEntry(full, short) {
-	let time = new Date(Date.now()).toISOString(); // TODO fix format
-	let tr = document.createElement('tr');
+	// Full URL
 	let c1 = document.createElement('td');
 	c1.innerText = full;
+
+	// Short URL
 	let c2 = document.createElement('td');
 	c2.setAttribute('data-short', short);
-	c2.innerText = window.location.origin+'/'+short; // TODO href and select everything until last folder
+	c2.innerText = window.location.origin+'/'+short; // TODO href - don't assume we are in root web folder
+	
+	// Date
+	let time = new Date(Date.now());
 	let c3 = document.createElement('td');
-	c3.innerText = time;
+	c3.setAttribute('time', time.toISOString())
+	let month = time.getMonth().toString().padStart(2, 0);
+	let date = time.getDate().toString().padStart(2, 0);
+	c3.innerText = time.getFullYear()+'-'+month+'-'+date+' '
+		+time.getHours()+':'+time.getMinutes()+':'+time.getSeconds();
+
+	let tr = document.createElement('tr');
 	tr.appendChild(c1);
 	tr.appendChild(c2);
 	tr.appendChild(c3);
